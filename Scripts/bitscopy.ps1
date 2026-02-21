a09c74361dfa1a45c3d49df4a74de12ba68c58f2
CobaltHex <cobalthex@live.com>
2026-02-20 17:54:30 -0800

    Forgot function

--- Scripts/bitscopy.ps1
+++ Scripts/bitscopy.ps1
@@ -1,3 +1,16 @@
+function Format-FileSize {
+    param([ulong]$Bytes)
+
+    switch ($Bytes) {
+        { $_ -ge 1PB } { "{0:N2} PiB" -f ($Bytes / 1PB); break }
+        { $_ -ge 1TB } { "{0:N2} TiB" -f ($Bytes / 1TB); break }
+        { $_ -ge 1GB } { "{0:N2} GiB" -f ($Bytes / 1GB); break }
+        { $_ -ge 1MB } { "{0:N2} MiB" -f ($Bytes / 1MB); break }
+        { $_ -ge 1KB } { "{0:N2} KiB" -f ($Bytes / 1KB); break }
+        default        { "$Bytes B" }
+    }
+}
+
 function Wait-BITSCopy {
     param(
         [Parameter(Mandatory = $true, Position = 0)]
@@ -36,10 +49,12 @@ function Wait-BITSCopy {
                 Complete-BitsTransfer $job
                 Write-Progress -Activity "Copying files" -Completed
                 Write-Host -ForegroundColor Green "File copy complete! ($(Format-FileSize $size))"
+                Remove-Variable $job
             }
             'Error' {
                 $job | Format-List *
                 Remove-BitsTransfer $job
+                Remove-Variable $job
                 exit 1
             }
         }
@@ -50,7 +65,7 @@ function Wait-BITSCopy {
         if ($job -and $job.JobState -notin @('Transferred', 'Acknowledged'))
         {
             Remove-BitsTransfer $job -ErrorAction SilentlyContinue
-            Write-Host -Foreground Yellow "Canceled file copy"
+            Write-Host -ForegroundColor Yellow "Canceled file copy"
         }
     }
 }
